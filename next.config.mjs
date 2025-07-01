df62b9129beabd79d1ad2ed9f63088d6ecf08ab8
abhinesh03 <ay935962@gmail.com>
2025-07-01 17:29:17 +0530

    bug-resolve

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// old code 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
 
+
+// // on deployement to vercel this file need to be present
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   output: 'export', // Ensures static HTML export
 // };
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
+
+
 
