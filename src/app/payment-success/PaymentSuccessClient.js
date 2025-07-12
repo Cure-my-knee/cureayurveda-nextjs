bfca11162c66c9084ef709ebf201d837efe5a1a8
abhinesh03 <ay935962@gmail.com>
2025-07-12 19:05:39 +0530

    build-shiprocket

---
+++ src/app/payment-success/PaymentSuccessClient.js
@@ -0,0 +1,18 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import React from 'react';
+
+export default function PaymentSuccessClient() {
+  const searchParams = useSearchParams();
+  const txnId = searchParams.get('transaction_id');
+
+  return (
+    <div className="min-h-screen flex flex-col items-center bg-[#ffff] justify-center">
+      <h1 className="text-2xl font-bold text-green-600">Payment Successful!</h1>
+      <p className="mt-4 text-gray-600">
+        Transaction ID: {txnId || 'N/A'}
+      </p>
+    </div>
+  );
+}
